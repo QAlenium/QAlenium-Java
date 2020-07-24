cc847856657f56d3c3c846b56a20af3081010971
Gabriel Aguido Fraga <gfraga@stack8.com>
2020-07-24 13:14:15 -0400

    new test tesmplate

--- src/test/java/br/com/qalenium/tests/TestTemplate.java
+++ src/test/java/br/com/qalenium/tests/TestTemplate.java
@@ -3,6 +3,7 @@ package br.com.qalenium.tests;
 import br.com.qalenium.annotations.Api;
 import br.com.qalenium.annotations.Description;
 import br.com.qalenium.annotations.Mobile;
+import br.com.qalenium.annotations.StoryLink;
 import br.com.qalenium.annotations.Web;
 import org.junit.After;
 import org.junit.AfterClass;
@@ -39,6 +40,7 @@ public class TestTemplate {
     @Api
     @Description("This test is supposed to assure something else")
     @Ignore("In case you want exclude this test from some suit")
+    @StoryLink("In case we have a story link to associate with")
     public void testItself() {
 
     }
