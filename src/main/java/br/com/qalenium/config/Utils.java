5944acb73ac9e9d1740069d93c536620b560954f
Gabriel Aguido Fraga <gfraga@stack8.com>
2020-07-24 22:26:50 -0400

    added files to main folder

---
+++ src/main/java/br/com/qalenium/config/Utils.java
@@ -0,0 +1,32 @@
+package br.com.qalenium.config;
+
+import org.junit.rules.RuleChain;
+import org.junit.rules.TestRule;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+
+public class Utils {
+
+    public WebDriver getDriver() {
+        String os = System.getProperty("os.name");
+
+        if (os.contains("OS X")) {
+            System.setProperty("webdriver.chrome.driver", "./driver/mac/chromedriver");
+        } else {
+            System.setProperty("webdriver.chrome.driver", "./driver/windows/chromedriver.exe");
+        }
+
+        return new ChromeDriver();
+    }
+
+    public RuleChain buildRuleChain(List<TestRule> rules) {
+        RuleChain ruleChain = RuleChain.emptyRuleChain();
+        for (TestRule testRule : rules) {
+            ruleChain = ruleChain.around(testRule);
+        }
+        return ruleChain;
+    }
+
+}
---
+++ src/main/java/br/com/qalenium/tests/TestTemplate.java
@@ -0,0 +1,46 @@
+package br.com.qalenium.tests;
+
+import br.com.qalenium.annotations.StoryLink;
+import br.com.qalenium.annotations.TestClassDescription;
+import br.com.qalenium.annotations.TestDescription;
+import br.com.qalenium.rules.WebTestsRule;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+@TestClassDescription("This test class is supposed to assure something else")
+public class TestTemplate extends WebTestsRule /*extends ApiTestsRule*/ /*extends MobileTestsRule*/ {
+
+    @BeforeClass
+    public static void classSetup() {
+
+    }
+
+    @AfterClass
+    public static void classTearDown() {
+
+    }
+
+    @Before
+    public void testSetup() {
+
+    }
+
+    @After
+    public void testTearDown() {
+
+    }
+
+    @Test
+    //@Api
+    //@Web
+    //@Mobile
+    @TestDescription("This test is supposed to assure something else")
+    //@Ignore("In case you want exclude this test from some suit")
+    @StoryLink("In case we have a story link to associate with")
+    public void testItself() {
+
+    }
+}
