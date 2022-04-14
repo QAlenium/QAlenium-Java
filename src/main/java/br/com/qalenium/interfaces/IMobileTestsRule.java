084b20b700085643fefcec04b299455dd67afc14
Gabriel Aguido Fraga <gabriel_aguido@hotmail.com>
2022-04-14 12:24:55 -0400

    added new interface methods to start/stop server/client

--- src/main/java/br/com/qalenium/interfaces/IMobileTestsRule.java
+++ src/main/java/br/com/qalenium/interfaces/IMobileTestsRule.java
@@ -1,4 +1,18 @@
 package br.com.qalenium.interfaces;
 
+import io.appium.java_client.android.AndroidDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+
 public interface IMobileTestsRule {
+
+    void closeEmulator() throws IOException;
+    AndroidDriver startEmulator() throws MalformedURLException;
+    void closeSimulator() throws IOException;
+    RemoteWebDriver startSimulator() throws IOException;
+    void startServer();
+    void stopServer() throws IOException;
+
 }
