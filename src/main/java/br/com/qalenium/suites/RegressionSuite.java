af30b86906ad3801e82edd3189c7994cee840c76
Gabriel Aguido Fraga <gabriel_aguido@hotmail.com>
2022-06-24 22:47:11 -0400

    refactor

--- src/main/java/br/com/qalenium/suites/RegressionSuite.java
+++ src/main/java/br/com/qalenium/suites/RegressionSuite.java
@@ -1,10 +1,10 @@
 package br.com.qalenium.suites;
 
-import br.com.qalenium.tests.TemplateTest;
+import br.com.qalenium.tests.WebTemplateTest;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({TemplateTest.class})
+@Suite.SuiteClasses({WebTemplateTest.class})
 public class RegressionSuite {
 }
