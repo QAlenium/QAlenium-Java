4d8f24876d980d3c6be94574c9a162c75dce23b1
Gabriel Aguido Fraga <gabriel_aguido@hotmail.com>
2022-06-24 22:51:44 -0400

    added mobile and rest test templates

--- src/main/java/br/com/qalenium/tests/MobileTemplateTest.java
+++ src/main/java/br/com/qalenium/tests/MobileTemplateTest.java
@@ -1,2 +1,36 @@
-package br.com.qalenium.tests;public class MobileTemplateTest {
+package br.com.qalenium.tests;
+
+import br.com.qalenium.annotations.Mobile;
+import br.com.qalenium.annotations.StoryLink;
+import br.com.qalenium.annotations.TestClassDescription;
+import br.com.qalenium.annotations.TestDescription;
+import br.com.qalenium.rules.MobileTestsRule;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+@TestClassDescription("Assure something")
+public class MobileTemplateTest extends MobileTestsRule {
+
+    @Before
+    public void testSetup() {
+    }
+
+    @After
+    public void testTearDown() {
+    }
+
+    @Test
+    @Mobile
+    //@Ignore("Ignore running this tests")
+    @StoryLink("https://www.example.com/issues/1234")
+    @TestDescription("assure something else")
+    public void testTemplate() {
+        Assert.assertEquals("200", "200");
+    }
 }
+
+
+
+
